2c85b82197a5a8720d851b5ef594303d156fbfb0
JAZWii <jazwii2216@gmail.com>
2024-10-01 21:24:27 +0300

    Cleaning, Stlying and adding comments

--- app/app.py
+++ app/app.py
@@ -1,9 +1,14 @@
+# Import Flask module
 from flask import Flask
+
+# Initialize Flask application
 app = Flask(__name__)
 
+# Define the default route that returns "Hello Foodics!"
 @app.route('/')
 def home():
     return "Hello Foodics!"
 
+# Run the application on host 0.0.0.0 and port 8080
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
